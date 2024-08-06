a21054d8c1338c161d9db9b881fd5e31dbf8e7c0
Polister <yyla05@foxmail.com>
2024-08-06 18:40:33 +0800

    fix:解决跨域问题

--- os_djg/os_djg/settings.py
+++ os_djg/os_djg/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'os_backend'
+    'os_backend',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +124,36 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# 允许所有域名进行跨域请求
+CORS_ALLOW_ALL_ORIGINS = True
+
+# 只允许特定域名进行跨域请求
+# CORS_ALLOWED_ORIGINS = [
+#     "http://example.com",
+#     "http://another-example.com",
+# ]
+
+# 允许所有HTTP方法进行跨域请求
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# 允许所有HTTP头进行跨域请求
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
