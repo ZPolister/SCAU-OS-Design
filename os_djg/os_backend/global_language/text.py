1d6cbd651549b2b316204b99271fd54f0801a705
Polister <yyla05@foxmail.com>
2024-08-08 13:23:27 +0800

    feat: 增加重名检测，增加deldir（删除非空文件夹），优化代码结构，文本国际化

--- os_djg/os_backend/global_language/text.py
+++ os_djg/os_backend/global_language/text.py
@@ -3,9 +3,16 @@ Global Language
 """
 text = {
     'en': {
+        # 文件操作信息
         'disk.dir_not_found': 'Directory not found',
+        'disk.file_already_exists': 'File already exists',
+        'disk.file_created': 'File created',
         'disk.file_not_found': 'File not found',
         'disk.not_enough_space': 'Not enough space',
+        'disk.could_not_copy_directory': 'Could not copy directory',
+
+        'disk.parent_directory_not_found': 'Parent directory not found',
+        'disk.directory_already_exists': 'Directory already exists',
 
     },
 
