13272a018b0b4f4e75218821720dbb0b9c0939eb
Polister <yyla05@foxmail.com>
2024-09-23 23:32:11 +0800

    fix: 修改配置

--- os_djg/os_djg/settings.py
+++ os_djg/os_djg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-9b7+gcu%3jc)g&6i-!g4o_^gj2t&meqj@jr-f^fb5m(8ie0^w(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
